282dbca7fdb83770e2d8b291c8d1433db22152e9
darekbx <darek.baranczuk@gmail.com>
2022-01-02 16:18:01 +0100

    Fixed di

--- android_app/app/src/main/java/com/darekbx/temperaturemonitor/di/CommonModule.kt
+++ android_app/app/src/main/java/com/darekbx/temperaturemonitor/di/CommonModule.kt
@@ -1,6 +1,7 @@
 package com.darekbx.temperaturemonitor.di
 
 import androidx.room.Room
+import com.darekbx.temperaturemonitor.ble.NotificationUtil
 import com.darekbx.temperaturemonitor.repository.AppDatabase
 import com.darekbx.temperaturemonitor.repository.AppDatabase.Companion.DB_NAME
 import com.darekbx.temperaturemonitor.viewmodel.SensorViewModel
@@ -14,6 +15,7 @@ object CommonModule {
             Room.databaseBuilder(androidApplication(), AppDatabase::class.java, DB_NAME).build()
         }
         single { get<AppDatabase>().entityDao() }
+        single { NotificationUtil(androidApplication()) }
         viewModel { SensorViewModel(get()) }
     }
 }
